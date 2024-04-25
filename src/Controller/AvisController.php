36d061682f63074d384f0ae917351ddb1d7e8317
crepinoScript <crepinoscript12@gmail.com>
2024-04-25 17:40:12 +0000

    creation des routes dans AvisController

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -2,19 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Avis;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class AvisController extends AbstractController
 {
     /**
-     * @Route("/avis", name="app_avis")
+     * Affiche la liste de toutes les avis
+     * @Route("/reviews", name="review_index")
      */
     public function index(): Response
     {
         return $this->render('avis/index.html.twig', [
             'controller_name' => 'AvisController',
         ]);
+    }
+
+     /**
+     * Affiche le détail d'une review
+     * @Route("/review/{review}", name="review_show", methods={"GET"})
+     * @param Review $review
+     */
+    public function show(Avis $avis)
+    {
+    }
+
+    /**
+     * Traite la requête d'un formulaire de création de review
+     * @Route("/review", name="review_create", methods={"POST"})
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Affiche le formulaire d'édition d'une review (GET)
+     * Traite le formulaire d'édition d'une review (POST)
+     * @Route("/review/{review}/edit", name="review_edit", methods={"GET", "POST"})
+     * @param Avis $avis
+     */
+    public function edit(Avis $avis)
+    {
+    }
+
+    /**
+     * Supprime une review
+     * @Route("/review/{review}", name="review_delete", methods={"DELETE"})
+     */
+    public function delete(Avis $avis)
+    {
     }
 }
