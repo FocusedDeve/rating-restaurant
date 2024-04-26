e11409ab35c34f0563416081d551cd5dfa1a003d
crepinoScript <crepinoscript12@gmail.com>
2024-04-26 16:18:23 +0000

    creation de Dat aFixtures

--- src/Controller/ImageRestaurentController.php
+++ src/Controller/ImageRestaurentController.php
@@ -22,9 +22,9 @@ class ImageRestaurentController extends AbstractController
     /**
      * Affiche le détail d'une picture
      * @Route("/picture/{picture}", name="picture_show", methods={"GET"})
-     * @param RestaurantPicture $picture
+     * @param ImageRestaurent $picture
      */
-    public function show(RestaurantPicture $picture)
+    public function show(ImageRestaurent $picture)
     {
     }
 
@@ -40,18 +40,18 @@ class ImageRestaurentController extends AbstractController
      * Affiche le formulaire d'édition d'une picture (GET)
      * Traite le formulaire d'édition d'une picture (POST)
      * @Route("/picture/{picture}/edit", name="picture_edit", methods={"GET", "POST"})
-     * @param RestaurantPicture $picture
+     * @param ImageRestaurent $picture
      */
-    public function edit(RestaurantPicture $picture)
+    public function edit(ImageRestaurent $picture)
     {
     }
 
     /**
      * Supprime une picture
      * @Route("/picture/{picture}", name="picture_delete", methods={"DELETE"})
-     * @param RestaurantPicture $picture
+     * @param ImageRestaurent $picture
      */
-    public function delete(RestaurantPicture $picture)
+    public function delete(ImageRestaurent $picture)
     {
     }
 }
